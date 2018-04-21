66c093e3c3c487d0c3e21e9f7b7fa64ac64ad7fa
Андрей Зарубин <submindly@gmail.com>
2018-04-22 06:00:49 +0700

    simple var ready

--- third_search.py
+++ third_search.py
@@ -17,19 +17,58 @@ class OrgSearch(unittest.TestCase):
         driver.get('https://yandex.ru')
         try:
             self.assertIn('Яндекс', driver.title)
-            time.sleep(2)
+            driver.implicitly_wait(5)
             self.assertTrue(driver.find_element_by_name('text'))
             driver.find_element_by_name('text').send_keys('Тензор')
-            time.sleep(1)
+            driver.implicitly_wait(5)
             self.assertTrue(driver.find_element_by_xpath("//div[ancestor::div/@class='popup__content']"))
-            time.sleep(1)
+            driver.implicitly_wait(5)
             driver.find_element_by_name('text').send_keys(Keys.RETURN)
-            time.sleep(1)
+            driver.implicitly_wait(5)
             self.assertTrue(driver.find_element_by_xpath("//ul[ancestor::div/@class='content__left']"))
             self.assertEqual(driver.find_element_by_xpath("//a[ancestor::div/@class='organic__subtitle typo typo_type_greenurl'][1]").text, 'tensor.ru')
-            time.sleep(2)
-        except NoSuchElementException:
+            driver.implicitly_wait(5)
+        except NoSuchElementException as e:
             pass  # log
+
+    def test_image_view_in_ya(self):
+        driver = self.driver
+        driver.get('https://yandex.ru')
+        try:
+            self.assertIn('Яндекс', driver.title)
+            driver.implicitly_wait(5)
+            self.assertTrue(driver.find_element_by_link_text('Картинки'))
+            driver.find_element_by_link_text('Картинки').click()
+            self.assertEqual(driver.current_url, 'https://yandex.ru/images/')
+            driver.implicitly_wait(5)
+
+            driver.find_element_by_class_name('image__image').click()
+            driver.implicitly_wait(5)
+            self.image_check()
+            first_img_url = driver.find_element_by_xpath("//img[@class='image__image']").get_attribute('src')
+            first_img = driver.title
+            driver.implicitly_wait(5)
+
+            driver.find_element_by_class_name('layout__nav__right').click()
+            self.image_check()
+            second_img = driver.title
+            driver.implicitly_wait(5)
+            self.assertNotEqual(first_img, second_img)
+            driver.implicitly_wait(5)
+            driver.find_element_by_class_name('layout__nav__left').click()
+            driver.implicitly_wait(5)
+            self.assertEqual(first_img_url, driver.find_element_by_xpath("//img[@class='image__image']").get_attribute('src'))
+
+        except NoSuchElementException as e:
+            pass  # log
+
+    def image_check(self):
+        '''
+        Проверяем, что присутсвуют изображение и кнопки вперед-назад
+        '''
+        self.assertTrue(self.driver.find_element_by_xpath("//img[@class='image__image']"))
+        self.assertTrue(self.driver.find_element_by_class_name('layout__nav__left'))
+        self.assertTrue(self.driver.find_element_by_class_name('layout__nav__right'))
 
         def tearDown(self):
             self.driver.close()
